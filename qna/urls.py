65b2aba83012aa535598a8bee542f939fb05a0a0
Satyam Fouzdar <fouzdarsatyam4@gmail.com>
2020-03-31 00:54:53 +0530

    Basic Questions Application

--- qna/urls.py
+++ qna/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('questions/', views.questions, name="questions"),
-    path('latestquestions/', views.latestquestions, name="latestquestions")
+    path('latestquestions/', views.latestquestions, name="latestquestions"),
+    path('ask-a-question/', views.askaquestion, name='askaquestion'),
 ]
