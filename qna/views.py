28d257d31f8fd052eceef831a70a4ad8635a5fab
Satyam Fouzdar <fouzdarsatyam4@gmail.com>
2020-04-03 10:05:17 +0530

    Fixed the homepage view

--- qna/views.py
+++ qna/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from . import models
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def questions(request):
     questions = models.Question.objects.filter(author=request.user).all()
 
@@ -13,6 +15,7 @@ def questions(request):
     return render(request, 'qna/questions.html', context)
 
 
+@login_required
 def latestquestions(request):
     questions = models.Question.objects.all().order_by('-created_at')
 
